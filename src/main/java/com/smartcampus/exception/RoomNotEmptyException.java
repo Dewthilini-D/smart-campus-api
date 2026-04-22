1f7f10391ec81b3d20d3897b464ed2efa9deb2f6
ඔයාගේ නම <dewthilini6@gmail.com>
2026-04-22 09:03:11 +0530

    Create com.smartcampus.exception package and add RoomNotEmptyException.java file

---
+++ src/main/java/com/smartcampus/exception/RoomNotEmptyException.java
@@ -0,0 +1,22 @@
+package com.smartcampus.exception;
+
+public class RoomNotEmptyException extends RuntimeException {
+    
+    private final String roomId;
+    private final int sensorCount;
+    
+    public RoomNotEmptyException(String roomId, int sensorCount) {
+        super("Room '" + roomId + "' cannot be deleted because it still has " 
+                + sensorCount + " active sensor(s) assigned to it.");
+        this.roomId = roomId;
+        this.sensorCount = sensorCount;
+    }
+    
+    public String getRoomId() {
+        return roomId;
+    }
+    
+    public int getSensorCount() {
+        return sensorCount;
+    }
+}
