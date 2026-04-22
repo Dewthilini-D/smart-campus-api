7ca764cdd0047355d497a2d96be326f7def8c8d1
ඔයාගේ නම <dewthilini6@gmail.com>
2026-04-22 09:19:13 +0530

    Create com.smartcampus.filter package and add LoggingFilter.java file

---
+++ src/main/java/com/smartcampus/filter/LoggingFilter.java
@@ -0,0 +1,35 @@
+package com.smartcampus.filter;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+@Provider
+public class LoggingFilter implements ContainerRequestFilter, ContainerResponseFilter {
+    
+    private static final Logger LOGGER = Logger.getLogger(LoggingFilter.class.getName());
+    
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        String method = requestContext.getMethod();
+        String uri = requestContext.getUriInfo().getRequestUri().toString();
+        
+        LOGGER.info("REQUEST : " + method + " " + uri);
+    }
+    
+    @Override
+    public void filter(ContainerRequestContext requestContext, 
+                       ContainerResponseContext responseContext) throws IOException {
+        
+        String method = requestContext.getMethod();
+        String uri = requestContext.getUriInfo().getRequestUri().toString();
+        int status = responseContext.getStatus();
+        
+        LOGGER.info("RESPONSE: " + method + " " + uri + " -> " + status);
+    }
+}
